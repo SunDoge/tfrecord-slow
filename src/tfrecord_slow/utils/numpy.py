e515a82561652f9a06d11ad959e8770daf836f2f
SunDoge <triplez0@outlook.com>
2024-04-23 16:56:10 +0800

    save

--- src/tfrecord_slow/utils/numpy.py
+++ src/tfrecord_slow/utils/numpy.py
@@ -1,6 +1,16 @@
 import numpy as np
-from typing import List
+from typing import List, Optional
 
 
-def from_buffer(buf: memoryview, shape: List[int], dtype: np.dtype) -> np.ndarray:
-    return np.frombuffer(buf, dtype=dtype).copy().reshape(shape)
+def from_buffer(
+    buf: memoryview,
+    shape: List[int] | None = None,
+    dtype: np.dtype = np.uint8,
+    copy: bool = True,
+) -> np.ndarray:
+    x = np.frombuffer(buf, dtype=dtype)
+    if copy:
+        x = x.copy()
+    if shape is not None:
+        x = x.reshape(shape)
+    return x
